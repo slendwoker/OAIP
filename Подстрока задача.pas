ec321ccc62aaa68369caf7aac65502d6c529a541
slendwoker <117628638+slendwoker@users.noreply.github.com>
2022-11-17 13:27:46 +0300

    Add files via upload

---
+++ Подстрока задача.pas
@@ -0,0 +1,4 @@
+﻿ var m = array [1..5, 1..5] of integer;
+ begin 
+   
+ end.
---
+++ возрастающий  мас 123131222313123123123.pas
@@ -0,0 +1,23 @@
+﻿begin
+var a:array[1..5] of integer;
+begin
+for var i := 1 to 5 do
+ begin
+  readln(A[i]);
+ end;
+  print(a);
+ var m:=0;
+ var k:=1;
+ var i1:=0;
+ for var i:=2 to 5 do
+   begin
+   if a[i]>=a[i-1] then k:=k+1;
+   i1:=i;
+   end;
+ begin
+  if i1=k then
+    print('YES')
+  else print('No');
+ end;
+end;
+end.
