7951966d369197690cb31a259aab95a0f22d2efa
slendwoker <117628638+slendwoker@users.noreply.github.com>
2022-12-05 10:51:56 +0300

    Add files via upload

---
+++ 10 lab 5 zad.pas
@@ -0,0 +1,20 @@
+﻿var A:array[1..8,1..8] of integer;
+var B:array[1..8] of integer;
+var sr:array[1..8] of integer;
+var i,j,c,d,max,sum:integer;
+begin
+  for i:=1 to 8 do
+    for j:= 1 to 8 do
+      A[i,j]:=random(1)-1;
+    writeln(A);
+    c:=1;
+   while c<=8 do begin
+     max:=1;
+     for d:=2 to 8 do
+       if A[c,d-1]=A[c,d] then max:=max+1; 
+     if max=8 then sr[c]:=1
+     else sr[c]:=-1;
+       c:=c+1;
+       end;
+       print(sr);
+end.
---
+++ 9 laba 1 zad.pas
@@ -0,0 +1,16 @@
+﻿function proz(x,y: integer) := x*y;
+const n=5;
+var a:array[1..n]of integer;
+var i,x,y:integer;
+begin 
+  for i:=1 to n do
+ readln(a[i]);
+x:=1;
+for i:=1 to n do 
+  if i mod 2 = 0  then 
+  begin
+    y:=a[i];
+  x:=proz(x,y);
+  end;
+  Writeln(x);
+end.
